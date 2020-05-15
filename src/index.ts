5cc762880bdc67874b3a11d9715d0f33fb6a8518
NG-Joseph <jcool4liv@gmail.com>
2020-05-15 10:58:34 +0100

    completed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import {log} from './tools';
 
+import Student from './student';
+import {PersonData, StudentData, Gender, ModeOfEntry} from './type-definition' 
+
 //Accessing object properties
 /*let firstName = "Pius";
 let lastName = "Onobhayedo";
@@ -11,7 +14,7 @@ let isAlive = true;
 let person1 = new Person(firstName, lastName, gender, height,
  hobbies, birthday, isAlive)
 log(`Person 1 is ${person1.firstName} whose height is ${person1.height} and birthday is $ {person1.birthday.toLocaleDateString()}`); */
-import {Person, PersonData} from './tools';
+/*import {Person, PersonData} from './tools';
 enum Gender{
  female = 1,
  male = 2
@@ -36,4 +39,30 @@ let person1 = new Person(person1Data);
 let person2 = new Person(person2Data);
 log(`Person 1 is ${person1.height} whose height is ${person1.height} and birthday is ${person1.birthday.toLocaleDateString()}`);
 log(`Person 1 is ${person1.getFullName()} whose height is ${person1.height}and birthday is ${person1.birthday.toLocaleDateString()} and hobbies are ${person1.hobbies.length==0?'not yet set':person1.hobbies}. <br/>
-Person 2 is ${person2.getFullName()} whose height is ${person2.height} and hobbies are ${person2.hobbies.length==0?'not yet set':person2.hobbies}`); 
+Person 2 is ${person2.getFullName()} whose height is ${person2.height} and hobbies are ${person2.hobbies.length==0?'not yet set':person2.hobbies}`); */
+//Student registration
+//prepare personal data
+let student1PersonalData: PersonData = {
+    firstName: "Mary",
+    lastName: "Ota",
+    gender: Gender.female,
+    hobbies: ['Work','Pray','Play'],
+    isAlive: true
+   }
+   //prepare student data
+   let student1StudentData: StudentData = {
+    department: "information science and media studies",
+    matriculationNumber: "234234",
+    modeOfEntry: ModeOfEntry.UTME,
+    programOfStudy: "Information Science and Media Studies",
+    yearOfEntry: new Date()
+   }
+   //instantiate the new student
+   let student1 = new Student(student1StudentData, student1PersonalData)
+   //access the new student’s data
+   log(`The Student 1 named ${student1.getFullName()} with matriculation number ${student1.matriculationNumber} <br/>
+   belongs to the department ${student1.department}. The height is ${student1.height}`); 
+   //add height to student1
+student1.height = 1.7;
+log(`The Student 1 named ${student1.getFullName()} with matriculation number ${student1.matriculationNumber} <br/>
+belongs to the department of ${student1.department}. The height is ${student1.height}`); 
